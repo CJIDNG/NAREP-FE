8b30bfab0709ef008c844acb665f7f0df59faf8b
odunayo <okebunmiodunayo@gmail.com>
2020-03-16 10:47:38 +0100

    setup redux saga

---
+++ src/redux/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../root.reducers';
+import rootSaga from '../root.sagas';
+
+const enhancers = [];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+  const { __REDUX_DEVTOOLS_EXTENSION__ } = window;
+  if (typeof __REDUX_DEVTOOLS_EXTENSION__ === 'function') {
+    enhancers.push(__REDUX_DEVTOOLS_EXTENSION__());
+  }
+}
+
+const composedEnhancers = compose(applyMiddleware(...middlewares), ...enhancers);
+export const store = createStore(rootReducer, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
