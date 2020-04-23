f92e85d05e5aef8bcddb365bc61224372161d681
odunayo <okebunmiodunayo@gmail.com>
2020-04-23 02:05:20 +0100

    added mobile responsiveness to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import Spinner from '@Atoms/spinner/spinner.component';
 import { ToastContainer } from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import BaseRoute from '@Routes/routes';
 
--- src/components/UI/organisms/header/header.component.jsx
+++ src/components/UI/organisms/header/header.component.jsx
@@ -1,116 +1,83 @@
-/* eslint-disable no-return-assign */
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Menu } from 'semantic-ui-react';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '@Redux/user/user.selectors';
 import { signOutStart } from '@Redux/user/user.action';
-import { Container } from './header.styles';
 
-const Header = ({ currentUser, signOutStart: logOutUser }) => {
+const Header = ({ currentUser, logOutUser }) => {
   const role = currentUser ? currentUser.role : null;
   const menuStyles = {
     color: '#fff',
-    fontSize: '1.2rem'
+    fontSize: '1.2rem',
+    marginRight: '10px',
   };
   const authStyles = {
     background: 'white',
-    color: '#000',
+    color: 'teal',
+    borderRadius: '5px',
+    width: '10rem',
+    textAlign: 'center',
+    marginRight: '50px',
+  };
+  const signupStyles = {
+    border: '1px solid white',
+    color: 'white',
+    borderRadius: '5px',
+    padding: '5px 15px',
+    lineHeight: '27px',
+    fontSize: '1.2rem',
+    width: '8rem',
+    textAlign: 'center',
+  };
+  const containerStyles = {
+    background: 'teal',
+    padding: '1.5rem 2rem'
   };
   return (
-    <>
-      <Container>
-        <Menu secondary className="mr-5">
-          <Menu.Item
-            name="home"
-            as={Link}
-            to="/"
-            style={menuStyles}
-          />
-          <Menu.Item
-            name="datasets"
-            as={Link}
-            to="/datasets"
-            style={menuStyles}
-          />
-          <Menu.Item
-            name="developers"
-            style={menuStyles}
-            onClick={() => window.open('https://github.com/CJIDNG/NAREP-BE')}
 
-          />
-          <Menu.Item
-            name="FAAC Facts"
-            style={menuStyles}
-            onClick={() => window.open('https://faacfacts.ptcij.org/')}
-          />
-          <Menu.Item
-            name="Policy Papers"
-            style={menuStyles}
-            as={Link}
-            to="/policy-papers"
-          />
-          <Menu.Item
-            name="blog"
-            as={Link}
-            style={menuStyles}
-            to="/blog"
-          />
-
-          {
-            currentUser ? (
-              <Menu.Menu position="right">
-                {
-                  role === 'admin' ? (
-                    <Menu.Item
-                      name="Admin Dashboard"
-                      as={Link}
-                      to="/admin-dashboard"
-                      style={authStyles}
-                    />
-                  ) : ''
-                }
-
-                <Menu.Item
-                  name="logout"
-                  as={Link}
-                  to="/logout"
-                  onClick={logOutUser}
-                  style={authStyles}
-
-                />
-              </Menu.Menu>
-            ) : (
-                <Menu.Menu position="right">
-                  <Menu.Item
-                    name="signin"
-                    as={Link}
-                    to="/signin"
-                    style={menuStyles}
-
-                  />
-                  <Menu.Item
-                    name="register"
-                    as={Link}
-                    to="/signup"
-                    style={authStyles}
-
-                  />
-                </Menu.Menu>
-            )
-          }
-        </Menu>
-      </Container>
-    </>
+    <Navbar collapseOnSelect expand="lg" style={containerStyles}>
+      <Navbar.Brand as={Link} to="/" style={menuStyles}>Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/datasets" style={menuStyles}>Datasets</Nav.Link>
+          <Nav.Link href="https://github.com/CJIDNG/NAREP-BE" target="_blank" style={menuStyles}>Developers</Nav.Link>
+          <Nav.Link href="https://faacfacts.ptcij.org/" target="_blank" style={menuStyles}>FAAC Facts</Nav.Link>
+          <Nav.Link as={Link} to="/policy-papers" style={menuStyles}>Policy Papers</Nav.Link>
+          <Nav.Link style={menuStyles} href="/blog">Blog</Nav.Link>
+        </Nav>
+        {
+          currentUser ? (
+            <Nav>
+              {
+                role === 'admin' ? (
+                  <Nav.Link as={Link} to="/admin-dashboard" style={authStyles}>Admin Dashboard</Nav.Link>
+                ) : null
+              }
+              <Button variant="outline-light" onClick={logOutUser}>Logout</Button>
+            </Nav>
+          ) : (
+              <Nav>
+                <Nav.Link as={Link} to="/signin" style={menuStyles}>Sign In</Nav.Link>
+                <Nav.Link as={Link} to="/signup" style={signupStyles}>Register</Nav.Link>
+              </Nav>
+          )
+        }
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
+
 Header.propTypes = {
   currentUser: PropTypes.shape({
     role: PropTypes.string
   }),
-  signOutStart: PropTypes.func.isRequired,
+  logOutUser: PropTypes.func.isRequired,
 };
 Header.defaultProps = {
   currentUser: null
@@ -119,6 +86,6 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 const mapDispatchToProps = (dispatch) => ({
-  signOutStart: () => dispatch(signOutStart())
+  logOutUser: () => dispatch(signOutStart())
 });
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
